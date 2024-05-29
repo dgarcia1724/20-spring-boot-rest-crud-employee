2e2cd622a871043a01387fa2ec2aaca0959e8189
Danny Garcia <dgarcia1724@berkeley.edu>
2024-05-28 21:30:05 -0700

    delete employee

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -50,6 +50,19 @@ public class EmployeeRestController {
 
         return dbEmployee;
     }
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
+
 }
 
 
